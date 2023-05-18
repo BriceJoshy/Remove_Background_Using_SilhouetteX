f010d9589d06ee4255dbdbadc5ea6db2499b32dc
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-18 23:29:43 +0530

    added an example image

--- python.py
+++ python.py
@@ -1,1 +1,4 @@
-print("hi")
+# image segmentation is the task of labelling each pixel of an image with a specific class
+# background:0
+# foreground:1
+"""Using the deeplabv3+ model silhoutteX.h5"""
