c6c64aca4fe32de0840b3f094462372d7b2b6fa9
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-31 14:39:13 +0530

    adding the threshold for better edge detection

--- sharpen.py
+++ sharpen.py
@@ -10,6 +10,9 @@ image.show()
 filter = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
 filter.show()
 
+ret, threshold = cv.threshold(filter, 1, 255, cv.THRESH_OTSU)
+cv.imshow("Threshold image", threshold)
+
 edges = filter.filter(ImageFilter.FIND_EDGES)
 edges.show()
 
